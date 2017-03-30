5607971752f97ac1017a910900537a52a38a0c62
Alexandru Mereacre <mereacre@gmail.com>
2017-03-30 14:13:00 +0100

    Added updated schema

--- process.js
+++ process.js
@@ -3,20 +3,22 @@ const _ = require("lodash");
 const fs = require('fs');
 
 // Ensure the rigth amount of arguments
-if (process.argv.length < 6) {
-    console.log("Usage: node process.js infile.csv outfile.json serviceId year")
+if (process.argv.length < 5) {
+    console.log("Usage: node process.js infile.csv outfile.json serviceId")
     process.exit(1);
 }
 
 // Non workforce types
 const typeList = [
-    "type",
-    "subType",
-    "activity",
+    "conditionType",
+    "condition",
+    "function",
 ];
 
 const NO_SKILL = "N";
 const NO_TYPE = "none";
+const currentTimestamp = Date.now();
+
 fs.open(process.argv[3], "w", (err, fd) => {
     if (err) {
         console.log(err);
@@ -33,10 +35,10 @@ fs.open(process.argv[3], "w", (err, fd) => {
                     
                     const jsonEntry = {
                         serviceId: process.argv[4],
-                        year: process.argv[5],
-                        type: entryCsv.type.length ? entryCsv.type : NO_TYPE,
-                        subType: entryCsv.subType.length ? entryCsv.subType : NO_TYPE,
-                        activity: entryCsv.activity.length ? entryCsv.activity : NO_TYPE,
+                        date: currentTimestamp,
+                        conditionType: entryCsv.conditionType.length ? entryCsv.conditionType : NO_TYPE,
+                        condition: entryCsv.condition.length ? entryCsv.condition : NO_TYPE,
+                        function: entryCsv.function.length ? entryCsv.function : NO_TYPE,
                     };
 
                     // Iterate over the types and filter the workforce types
@@ -45,8 +47,8 @@ fs.open(process.argv[3], "w", (err, fd) => {
                         if (typeList.indexOf(key) < 0) {
                             jsonEntry.workforceType = key;
                             if (value.length)
-                                jsonEntry.skill = value;
-                            else jsonEntry.skill = NO_SKILL;
+                                jsonEntry.competent = value;
+                            else jsonEntry.competent = NO_SKILL;
 
                             const jsonStr = JSON.stringify(jsonEntry)+"\n";
                             fs.writeFileSync(process.argv[3], jsonStr, {flag: "a"}, (err) => {
